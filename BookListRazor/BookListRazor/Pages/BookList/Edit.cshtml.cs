2317a8f3a977bb47d7ed37a80f011495deffb0cb
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-01 02:13:24 +0200

    42. Edit post handler

--- BookListRazor/BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -18,11 +18,31 @@ namespace BookListRazor.Pages.BookList
             _context = context;
         }
 
+        [BindProperty]
         public Book Book { get; set; }
 
         public async Task OnGet(int id)
         {
             Book = await _context.Books.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var bookFromDb = await _context.Books.FindAsync(Book.Id);
+                if (bookFromDb is not null)
+                {
+                    bookFromDb.Name = Book.Name;
+                    bookFromDb.Author = Book.Author;
+                    bookFromDb.ISBN = Book.ISBN;
+                    await _context.SaveChangesAsync();
+                    return RedirectToPage("Index");
+                }
+                return Page();
+            }
+
+            return Page();
         }
     }
 }
